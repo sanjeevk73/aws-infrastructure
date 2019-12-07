c596b89cd826a524784ec052ce5839600978ad4d
sanjeevk73 <58186586+sanjeevk73@users.noreply.github.com>
2019-12-07 14:07:42 -0600

    Update deploy_stack.sh

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,8 +1,8 @@
 
 
 
-cd templates
-aws s3 sync . s3://$2
-aws s3 ls s3://$2
+#cd templates
+#aws s3 sync . s3://$2
+#aws s3 ls s3://$2
 
-aws cloudformation create-stack --stack-name $1 --template-url https://s3.amazonaws.com/$2/$3 --parameters https://s3.amazonaws.com/$2/$4 --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --region us-east-1 --stack-name $1 --template-body file://home/ec2-user/aws-infrastructure/templates/$2 --parameters ParameterKey=KeyName,ParameterValue=VS_EC2  --capabilities CAPABILITY_IAM
