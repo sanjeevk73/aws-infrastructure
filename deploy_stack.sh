fde36a1250e385f08b7becb0b5c1705e3cf4ea70
Sanjeev Kumar <KumarSanjeev@JohnDeere.com>
2019-12-06 23:51:04 -0600

    fixed deploy stack script

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -5,8 +5,4 @@ cd templates
 aws s3 sync . s3://$2
 aws s3 ls s3://$2
 
-aws cloudformation create-stack \ 
---stack-name $1 \
---template-url https://s3.amazonaws.com/$2/$3 \
---parameters https://s3.amazonaws.com/$2/$4 \
---capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name $1 --template-url https://s3.amazonaws.com/$2/$3 --parameters https://s3.amazonaws.com/$2/$4 --capabilities CAPABILITY_IAM
